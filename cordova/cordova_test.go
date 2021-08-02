d1c7289ca637a4e9be14030330fe521b4de0865d
Olivér Falvai <oliver.falvai@bitrise.io>
2021-08-02 14:33:39 +0200

    Add APK/AAB config input

---
+++ cordova/cordova_test.go
@@ -0,0 +1,63 @@
+package cordova
+
+import (
+	"testing"
+)
+
+func Test_platformCustomOptions(t *testing.T) {
+	tests := []struct {
+		name    string
+		model   Model
+		wantCmd string
+	}{
+		{
+			"Project with generic option and AAB output",
+			*New().SetCustomOptions("--release").SetPlatforms("android").SetAndroidAppType("aab"),
+			`cordova "compile" "android" "--release" "--" "--packageType=bundle"`,
+		},
+		{
+			"Project with generic option and APK output",
+			*New().SetCustomOptions("--release").SetPlatforms("android").SetAndroidAppType("apk"),
+			`cordova "compile" "android" "--release" "--" "--packageType=apk"`,
+		},
+		{
+			"Project with no generic option and APK output",
+			*New().SetPlatforms("android").SetAndroidAppType("apk"),
+			`cordova "compile" "android" "--" "--packageType=apk"`,
+		},
+		{
+			"Project with no generic option and AAB output",
+			*New().SetPlatforms("android").SetAndroidAppType("aab"),
+			`cordova "compile" "android" "--" "--packageType=bundle"`,
+		},
+		{
+			"Project with platform-specific and generic options and AAB output",
+			*New().SetCustomOptions("--release", "--", "--keystore=android.keystore").SetPlatforms("android").SetAndroidAppType("aab"),
+			`cordova "compile" "android" "--release" "--" "--packageType=bundle" "--keystore=android.keystore"`,
+		},
+		{
+			"Project with platform-specific and generic options and APK output",
+			*New().SetCustomOptions("--release", "--", "--keystore=android.keystore").SetPlatforms("android").SetAndroidAppType("apk"),
+			`cordova "compile" "android" "--release" "--" "--packageType=apk" "--keystore=android.keystore"`,
+		},
+		{
+			"Project with only packageType option and APK output",
+			*New().SetCustomOptions("--", `--packageType="bundle"`).SetPlatforms("android").SetAndroidAppType("apk"),
+			`cordova "compile" "android" "--" "--packageType=apk" "--packageType=\"bundle\""`,
+		},
+		{
+			"Project with only packageType option and AAB output",
+			*New().SetCustomOptions("--", `--packageType="bundle"`).SetPlatforms("android").SetAndroidAppType("bundle"),
+			`cordova "compile" "android" "--" "--packageType=bundle" "--packageType=\"bundle\""`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := tt.model.CompileCommand().PrintableCommandArgs()
+			if tt.wantCmd != cmd {
+				t.Errorf("Cordova command doesn't match wanted value.\nWant: %v\nGot: %s", tt.wantCmd, cmd)
+			}
+		})
+	}
+}
